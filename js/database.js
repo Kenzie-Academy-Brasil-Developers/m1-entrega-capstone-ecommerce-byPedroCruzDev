5c40d89635c9c889ceb603d3c41ca5449bfad4c2
PedroCruz <contatocreed67@gmail.com>
2022-07-20 12:19:47 -0400

    Cards add pelo dom, cards add ao carrinho

--- js/database.js
+++ js/database.js
@@ -3,124 +3,156 @@
 const data = [
   {
     id: 1,
-    img: "../img/01.jpg",
+    img: "img/01.jpg",
     nameItem: "One Piece 3 em 1",
     description:
       "Luffy deseja tornar-se um pirata por conta da admiração que sente por Shanks. O garoto acaba consumindo a Gomugomu-no-mimais e mais tarde se envolve em uma briga com...",
-    value: 50,
+    value: 49.99,
     addCart: "Adicionar ao carrinho",
     tag: ["Manga"],
   },
   {
     id: 2,
-    img: "../img/02.jpg",
+    img: "img/02.jpg",
     nameItem: "Chainsaw Man Vol.1",
     description:
       "Denji é um jovem extremamente pobre que junto de Pochita, seu demônio de estimação, trabalha feito um condenado como Caçador de Demônios para pagar a imensa dívida...",
-    value: 60,
+    value: 59.99,
     addCart: "Adicionar ao carrinho",
     tag: ["Manga"],
   },
   {
     id: 3,
-    img: "../img/03.jpg",
+    img: "img/03.jpg",
     nameItem: "Jujutsu Kaisen: Vol.1",
     description:
       "Apesar do estudante colegial Yuuji Itadori ter grande força física, ele se inscreve no Clube de Ocultismo. Certo dia, eles encontram um objeto amaldiçoado e...",
-    value: 40,
+    value: 39.99,
     addCart: "Adicionar ao carrinho",
     tag: ["Manga"],
   },
   {
     id: 4,
-    img: "../img/04.jpg",
+    img: "img/04.jpg",
     nameItem: "Kimetsu No Yaiba Vol.1",
     description:
       "Produto em pré-venda, tendo início de postagem a partir da 2º quinzena do mês de Março/20! Estamos na Era Taishou. O dia-a-dia pacato de Tanjiro, um gentil...",
-    value: 70,
+    value: 69.99,
     addCart: "Adicionar ao carrinho",
     tag: ["Manga"],
   },
   {
     id: 5,
-    img: "../img/05.jpg",
+    img: "img/05.jpg",
     nameItem: "Boku no Hero - Vol.2",
     description:
       "Vai começar a simulação de combate real! Deku precisa reunir toda a sua coragem para enfrentar os desafios que virão pela frente. E o primeiro adversário...",
-    value: 60,
+    value: 59.99,
     addCart: "Adicionar ao carrinho",
     tag: ["Manga"],
   },
   {
     id: 6,
-    img: "../img/06.jpg",
+    img: "img/06.jpg",
     nameItem: "Soul Eater Vol. 1",
     description:
       "Na escola Shibusen, localizada no estado americano de Nevada, munidos de suas respectivas armas, as artesãs e os artesãos Maka Albarn e Soul Eater Evans; Black Star e...",
-    value: 40,
+    value: 39.99,
     addCart: "Adicionar ao carrinho",
     tag: ["Manga"],
   },
   {
     id: 7,
-    img: "../img/07.jpg",
+    img: "img/07.jpg",
     nameItem: "Inuyasha Vol. 01",
     description:
       "A jovem Kagome encontra em um antigo poço uma passagem que a leva para um Japão Medieval habitado por criaturas místicas conhecidas como youkais. É nesse passado...",
-    value: 40,
+    value: 39.99,
     addCart: "Adicionar ao carrinho",
     tag: ["Manga"],
   },
   {
     id: 8,
-    img: "../img/08.jpg",
+    img: "img/08.jpg",
     nameItem: "Hunter X Hunter Vol.26",
     description:
       "Finalmente o Presidente Netero está frente a frente com o Rei das Chimera Ant e o mesmo vale para Gon e Pitou. Porém, tanto o jovem herói quanto o líder dos Hunters...",
-    value: 50,
+    value: 49.99,
     addCart: "Adicionar ao carrinho",
     tag: ["Manga"],
   },
   {
     id: 9,
-    img: "../img/09.webp",
+    img: "img/09.webp",
     nameItem: "Inumanos - capa dura",
     description:
-      "OS INUMANOS COMO VOCÊ NUNCA VIU! Os Inumanos são uma das esquisitices mais duradouras da Marvel. Uma raça de anomalias genéticas isolada na ilha de Attilan, suas mutações...",
-    value: 43.90,
+      "OS INUMANOS COMO VOCÊ NUNCA VIU! Os Inumanos são uma das esquisitices mais duradouras da Marvel. Uma raça de anomalias genéticas isolada na ilha de Attilan, suas...",
+    value: 43.99,
     addCart: "Adicionar ao carrinho",
     tag: ["Hq"],
   },
   {
     id: 10,
-    img: "../img/10.webp",
-    nameItem: "Miles Morales: Homem Aranha",
+    img: "img/10.webp",
+    nameItem: "Miles Morales vol 1",
     description:
-      "Quando o enfurecido Rino e um grupo de criminosos misteriosos começam a atormentar o Brooklyn, as coisas ficam feias para o jovem Homem-Aranha! Que mistérios o aguardam...",
-    value: 25.90,
+      "Quando o enfurecido Rino e um grupo de criminosos misteriosos começam a atormentar o Brooklyn, as coisas ficam feias para o jovem Homem-Aranha! Que mistérios...",
+    value: 25.99,
     addCart: "Adicionar ao carrinho",
     tag: ["Hq"],
   },
   {
     id: 11,
-    img: "../img/11.webp",
+    img: "img/11.webp",
     nameItem: "A Ordem Mágica",
     description:
       "Há um motivo pelo qual você nunca viu um fantasma.De dia, os membros da Ordem Mágica vivem entre nós como amigos, vizinhos e colegas de trabalho...",
-    value: 64.90,
+    value: 64.99,
     addCart: "Adicionar ao carrinho",
     tag: ["Hq"],
   },
   {
     id: 12,
-    img: "../img/12.webp",
+    img: "img/12.webp",
     nameItem: "Deadpool - Antologia",
     description:
-      "O programa Arma X atribui a Wade Wilson um fator de regeneração que lhe permite curar-se de um câncer, mas o deixa desfigurado para sempre...",
-    value: 126.90,
+      "O programa Arma X atribui a Wade Wilson um fator de regeneração que lhe permite curar-se de um câncer, mas o deixa desfigurado...",
+    value: 126.99,
     addCart: "Adicionar ao carrinho",
     tag: ["Hq"],
   },
   
   
 ];
+
+//interação da barra de pesquisa e do carrinho
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+//container de pesquisa
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
--- js/index.js
+++ js/index.js
@@ -1,31 +1,197 @@
-let navbar = document.querySelector('.navbar');
 
-document.querySelector('#menu-btn').onclick = () =>{
-    navbar.classList.toggle('active');
-    searchForm.classList.remove('active');
-    cartItem.classList.remove('active');
+let menu = document.getElementById('menu')
+let ul   = document.createElement('ul')
+menu.append(ul)
+ul.classList = "box-container"
+
+let cartUl = document.querySelector('.cart-section')
+
+
+
+//loop do array de objetos
+
+function listProducts (arr, section){
+
+  section.innerHTML = ''
+
+    for(let i =0; i < arr.length; i++){
+        
+        let products = arr[i]
+        createCards(products, section)
+    }
+}
+listProducts(data, ul)
+
+function createCards(obj, section){
+    let id          = obj.id
+    let image       = obj.img
+    let name        = obj.nameItem
+    let discription = obj.description
+    let price       = obj.value
+    let addCart     = obj.addCart
+    let tag         = obj.tag
+    
+    //criando tags
+    const li      = document.createElement('li')
+    const img     = document.createElement('img')
+    const h3      = document.createElement('h3')
+    const p       = document.createElement('p')
+    const h4      = document.createElement('h4')
+    const span    = document.createElement('span')
+    const btnA    = document.createElement('button')
+    const div     = document.createElement('div')
+
+    
+
+    if(section.classList == 'box-container'){
+    
+    //criando append
+    li.append(img, h3, p, h4, span, btnA)
+
+    section.append(li)
+
+    //adicionando classes
+    li.classList        = 'box'
+    h4.classList        = 'price'
+    btnA.classList   = 'btn'
+
+    //adicionando atributos
+
+    img.src         = image
+    h3.innerHTML    = name
+    p.innerHTML     = discription
+    h4.innerHTML    = `R$${price.toString().replace(".", ",")}`
+    span.innerHTML  = tag
+    btnA.innerHTML  = addCart
+    btnA.id         = id
+
+}else if(section.classList == 'cart-section'){
+    span.classList = 'fas fa-times'
+    li.classList   = 'cart-item'
+    div.classList  = 'content'
+    h4.classList   = 'price'
+
+    div.append(h3, h4)
+    li.append(span, img, div)
+    section.append(li)
+    span.id         = name
+    img.src         = image
+    h3.innerHTML    = name
+    h4.innerHTML    = `R$${price.toString().replace(".", ",")}`
+
+    }
 }
 
-let searchForm = document.querySelector('.search-form');
+ul.addEventListener('click', inteceptClick)
 
-document.querySelector('#search-btn').onclick = () =>{
-    searchForm.classList.toggle('active');
-    navbar.classList.remove('active');
-    cartItem.classList.remove('active');
+let cardProducts = []
+
+function inteceptClick(event){
+    let buyBtn = event.target
+
+    if(buyBtn.tagName == 'BUTTON' ){
+        //id do produto
+      let idProduct = buyBtn.id
+
+      let products = data.find(function(products){
+
+            if(products.id == idProduct ){
+              
+                return products
+            }
+        })
+          addToCart(products) 
+    }
+   
 }
 
-let cartItem = document.querySelector('.cart-items-container');
+function addToCart(products){
+    if(products !== undefined){
+        cardProducts.push(products)
+        
+    }
+    listProducts(cardProducts,cartUl)
+    sumTotal(cardProducts)
+}
+console.log(cardProducts)
 
-document.querySelector('#cart-btn').onclick = () =>{
-    cartItem.classList.toggle('active');
-    navbar.classList.remove('active');
-    searchForm.classList.remove('active');
+let priceTotal = document.querySelector('.price > span')
+let totalItens = document.querySelector('.quantidade > span')
+
+
+function sumTotal (arr){
+    let sumItens = 0
+    let sumTotalItens = 0
+
+    for(let i = 0; i < arr.length; i ++ ){
+       let position = arr[i]
+ 
+
+        sumTotalItens = i + 1
+  
+        sumItens += position.value
+        
+    }
+    priceTotal.innerHTML = sumTotalItens
+    totalItens.innerHTML = `R$${sumItens.toString().replace(".", ",")}`
 }
 
-window.onscroll = () =>{
-    navbar.classList.remove('active');
-    searchForm.classList.remove('active');
-    cartItem.classList.remove('active');
+cartUl.addEventListener('click', removeButton)
+
+function removeButton (event){
+
+    let spanButton = event.target
+
+    console.log(spanButton)
+
+
+    if(spanButton.tagName == 'SPAN'){
+
+      let index = cardProducts.findIndex((element) => {
+        return  element.name == spanButton.id
+       })
+  
+       console.log(index)
+       cardProducts.splice(index,1)
+
+     
+        
+    listProducts(cardProducts,cartUl)
+    sumTotal(cardProducts)
+    
+    console.log(cardProducts)
+    }
+
+    return cardProducts
 }
 
-console.dir(data)
+
+//barra de pesquisa 
+
+let input  = document.querySelector('#search-box')
+let button = document.querySelector('.search-form > label')
+
+
+button.addEventListener('click', (event) =>{
+    //value => valor digitado pelo usuario
+    let searchItens = input.value
+
+    let resultSearch= search(searchItens)
+
+    listProducts(resultSearch, ul)
+} )
+
+function search(value){
+    let resultToSearch = []
+
+    for(let i = 0; i<data.length; i ++){
+        if(value == data[i].nameItem){
+
+            resultToSearch.push(data[i])
+        }else if(value == data[i].tag){
+            resultToSearch.push(data[i])
+        }
+    }
+
+    return resultToSearch
+}
